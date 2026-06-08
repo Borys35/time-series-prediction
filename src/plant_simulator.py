8f7ddebcebf86b5421d16a221055179ef5170120
Borys35 <boryskac10@gmail.com>
2026-06-08 20:26:45 +0200

    fix db connection issue

--- src/plant_simulator.py
+++ src/plant_simulator.py
@@ -8,15 +8,19 @@ def start_plant_simulator():
     client = create_mqtt_client(client_id="plant_simulator")
     client.connect(BROKER_HOST, BROKER_PORT)
 
-    engine = create_engine('postgresql://postgres:postgres@localhost:5432/tep_db')
+    engine = create_engine('postgresql://postgres:postgres@127.0.0.1:5435/tep_db')
 
     testing_data = pyreadr.read_r('./datasets/tep/TEP_FaultFree_Testing.RData')
     df = pd.DataFrame(testing_data['fault_free_testing'])
+    df = df[df['simulationRun'] == 1]
 
     input_columns = [col for col in df.columns if col.startswith('xmeas')]
     df = df[input_columns]
 
+    print("Starting plant simulator...")
+
     for index, row in df.iterrows():
+        print(f"Inserting record {index + 1}/{len(df)} into the database...")
         row.to_sql('sensor_data', engine, if_exists='append', index=False)
         client.publish(TOPIC_SENSORS_OUTPUTS)
         time.sleep(1) # Sleep for 1 second before inserting the next record
