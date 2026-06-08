a51ebbaebca342141f82a9652ca66ea285ae3fe1
Borys35 <boryskac10@gmail.com>
2026-06-08 20:43:49 +0200

    add seed.sql. plant_simulator.py is working correctly

--- src/plant_simulator.py
+++ src/plant_simulator.py
@@ -1,6 +1,6 @@
 import time
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import pyreadr
 from mqtt_config import (create_mqtt_client, TOPIC_SENSORS_OUTPUTS, BROKER_HOST, BROKER_PORT)
 
@@ -9,6 +9,11 @@ def start_plant_simulator():
     client.connect(BROKER_HOST, BROKER_PORT)
 
     engine = create_engine('postgresql://postgres:postgres@127.0.0.1:5435/tep_db')
+
+    # clear existing data in the table
+    with engine.connect() as connection:
+        connection.execute(text("DELETE FROM sensor_data"))
+        connection.commit()
 
     testing_data = pyreadr.read_r('./datasets/tep/TEP_FaultFree_Testing.RData')
     df = pd.DataFrame(testing_data['fault_free_testing'])
@@ -21,7 +26,7 @@ def start_plant_simulator():
 
     for index, row in df.iterrows():
         print(f"Inserting record {index + 1}/{len(df)} into the database...")
-        row.to_sql('sensor_data', engine, if_exists='append', index=False)
+        pd.DataFrame([row]).to_sql('sensor_data', engine, if_exists='append', index=False)
         client.publish(TOPIC_SENSORS_OUTPUTS)
         time.sleep(1) # Sleep for 1 second before inserting the next record
 
